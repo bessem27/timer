73162bc6544edfccfded8e349227ce85a558d4e0
bessem27 <bessemkerkeni@gmail.com>
2019-07-14 11:07:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Time from './Time'
+
+const App = () => {
+  return <div>
+    <Time />
+  </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
